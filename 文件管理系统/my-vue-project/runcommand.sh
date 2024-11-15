6bf3e1097fcdf3f42340b10e4b207987c278d85a
Meiqi Huang <1053825066@qq.com>
2024-11-15 14:15:49 +0800

    update

--- 文件管理系统/my-vue-project/runcommand.sh
+++ 文件管理系统/my-vue-project/runcommand.sh
@@ -1,2 +1,2 @@
-docker run -d -p 8084:8080 -v .:/app my-vue-project
-docker run -d -p 8085:8080 -v .:/app my-vue-project
+docker run -d -p 8084:8082 -v .:/app my-vue-project
+docker run -d -p 8085:8082 -v .:/app my-vue-project
--- 文件管理系统/my-vue-project/vue.config.js
+++ 文件管理系统/my-vue-project/vue.config.js
@@ -3,5 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 8082,
+    host: '0.0.0.0',         // 绑定到所有可用的网络接口
+    allowedHosts: 'all',     // 允许所有主机访问
   },
 });
